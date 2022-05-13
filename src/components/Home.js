4e8e754ce358a201f224edbfbc1ce256d7921124
Harsh Prasad <harshisindian@gmail.com>
2022-05-13 13:17:59 +0530

    Implemented Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
 
-function Home(props) {
-  return <div>Home</div>;
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+
+        console.log("posts", posts);
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Harsh Prasad</div>
+
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
