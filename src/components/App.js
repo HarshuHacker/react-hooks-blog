4c6de02cc55dce6279f6b570ccdf4087d441529e
Harsh Prasad <harshisindian@gmail.com>
2022-05-13 12:53:52 +0530

    Completed CreatePost Using Custom Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { Switch, Route } from "react-router-dom";
 
-import { CreatePost, Home, Navbar, PostDetail } from "./";
+import { CreatePost, Home, Navbar, PostDetail } from "./index"
 
 function App() {
   return (
@@ -8,8 +8,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/post/:postId" component={PostDetail} />
-        <Route exact path="/create-post" component={CreatePost} />
+        <Route path="/post/:postId" component={PostDetail} />
+        <Route path="/create-post" component={CreatePost} />
       </Switch>
     </div>
   );
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,71 @@
+// import React, { useState } from "react";
 import React from "react";
+import { db } from "../firebase"
+import {useFormInput} from "../hooks"
 
 function CreatePost(props) {
-  return <div>CreatePost</div>;
+  // const [title, setTitle] = useState("");
+  // const [subTitle, setSubTitle] = useState("");
+  // const [content, setContent] = useState("");
+
+  const title = useFormInput("");
+  const subTitle = useFormInput("");
+  const content = useFormInput("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("title : ", title);
+    console.log("subTitle : ", subTitle);
+    console.log("content : ", content);
+
+    // db.collection("posts").add({
+    //   title,
+    //   subTitle,
+    //   content,
+    //   createdAt: new Date()
+    // })
+
+    db.collection("posts").add({
+      title: title.value,
+      subTitle: subTitle.value,
+      content: content.value,
+      createdAt: new Date()
+    })
+  }
+
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          {/* <input value={title} onChange={(e) => setTitle(e.target.value)} /> */}
+          <input {...title} />
+        </div>
+
+        <div className="form-field">
+          <label>Sub Title</label>
+          {/* <input
+            value={subTitle}
+            onChange={(e) => setSubTitle(e.target.value)}
+          /> */}
+          <input {...subTitle}/>
+        </div>
+
+        <div className="form-field">
+          <label>Content</label>
+          {/* <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea> */}
+          <textarea {...content} />
+        </div>
+
+        <button className="create-post-btn">Create Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
