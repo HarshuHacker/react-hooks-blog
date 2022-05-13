ff6bd9704649ff3525eef63221da2050123d1303
Harsh Prasad <harshisindian@gmail.com>
2022-05-13 19:55:45 +0530

    Styled Components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { Link } from "react-router-dom";
+import styledComponents from "styled-components";
+
+// we can assume it as a functional component
+const BlogHeading = styledComponents.h1`
+  text-align: center;
+  color: #2196f3;
+  margin-bottom: 2px;
+`;
+
+const PostSubtitle = styledComponents.p`
+  font-size: 13px;
+`;
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -23,15 +35,16 @@ function Home() {
 
   return (
     <div className="home">
-      <h1 style={styles.heading}>Tech Blog</h1> {/* inline styling */}
+      {/* inline styling */}
+      {/* <h1 style={styles.heading}>Tech Blog</h1>  */}
+      <BlogHeading>Tech Blog</BlogHeading>
       <div id="blog-by">Harsh Prasad</div>
       {posts.map((post, index) => (
         <div className="post" key={`post-${index}`}>
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
           </Link>
-
-          <p>{post.subTitle}</p>
+          <PostSubtitle>{post.subTitle}</PostSubtitle>
         </div>
       ))}
     </div>
@@ -40,9 +53,9 @@ function Home() {
 
 export default Home;
 
-const styles = {
-  heading: {
-    marginTop: 30, // px is by default
-    fontSize: 56,
-  },
-};
+// const styles = {
+//   heading: {
+//     marginTop: 30, // px is by default
+//     fontSize: 56,
+//   },
+// };
